89dc4ede6b60846ec886a7b88735dd741b65919d
Cris Cadinu <cadhoburg@gmail.com>
2021-12-08 09:42:29 +0100

    Refactored classes so that it works on different machines

--- src/test/java/datastructureTests/DequeueTest.java
+++ src/test/java/datastructureTests/DequeueTest.java
@@ -1,218 +1,212 @@
-//package datastructureTests;
-//
-//import datastructures.Deque;
-//import datastructures.IDequeue;
-//import datastructures.IStack;
-//import datastructures.StackCris;
-//import org.json.simple.JSONObject;
-//import org.json.simple.parser.ParseException;
-//import org.junit.Assert;
-//import org.junit.Before;
-//import org.junit.Test;
-//import utils.Dataset;
-//import utils.InputStreamResourceReaderStrategy;
-//import utils.JSONHandler;
-//import utils.ResourceReaderStrategy;
-//
-//import java.io.IOException;
-//import java.io.InputStream;
-//import java.time.Duration;
-//import java.time.Instant;
-//import java.util.ArrayList;
-//
-//public class DequeueTest {
-//    JSONHandler jsonHandler;
-//    JSONObject jsonObject;
-//    Dataset dataset;
-//    IDequeue<Integer> testDequeue;
-//
-//    @Before
-//    public void setup() throws IOException, ParseException {
-//        ResourceReaderStrategy<InputStream> reader = new InputStreamResourceReaderStrategy();
-//        jsonHandler = new JSONHandler();
-//        dataset = new Dataset();
-//        InputStream is = reader.getFileFromResource("datasets/sorting.json");
-//        jsonObject = jsonHandler.getJSONObjectFromInputStream(is);
-//        testDequeue = new Deque<>(4);
-//    }
-//
-//    @Test
-//    public void shouldInsertDataSetDoubleIntoDequeue() {
-//        ArrayList<Double> listWithDouble = dataset.getFloat8001();
-//        IDequeue<Double> doubleDequeue = new Deque<>(listWithDouble.size());
-//        Instant startTime;
-//        long delta;
-//
-//        int actual = listWithDouble.size();
-//
-//        startTime = Instant.now();
-//        for (Double element : listWithDouble) {
-//            doubleDequeue.enqueFront(element);
-//        }
-//        delta = Duration.between(startTime, Instant.now()).toMillis();
-//        System.out.println("Duration for inserting Double into Dequeue: " + delta + "ms");
-//
-//        int expected = doubleDequeue.size();
-//
-//        Assert.assertEquals(actual, expected);
-//    }
-//
-//    @Test
-//    public void shouldInsertDataSetLongIntoDequeue() {
-//        ArrayList<Long> listWithLong = dataset.getLijstHerhaald1000();
-//        IDequeue<Long> longDequeue = new Deque<>(listWithLong.size());
-//        Instant startTime;
-//        long delta;
-//
-//        int expected = listWithLong.size();
-//
-//        startTime = Instant.now();
-//        for (Long element : listWithLong) {
-//            longDequeue.enqueFront(element);
-//        }
-//        delta = Duration.between(startTime, Instant.now()).toMillis();
-//
-//        System.out.println("Duration for inserting Long into Dequeue: " + delta + "ms");
-//        int actual = longDequeue.size();
-//
-//        Assert.assertEquals(expected, actual);
-//    }
-//
-//    @Test
-//    public void shouldInsertDataSetStringsIntoDequeue() {
-//        ArrayList<String> listWithStrings = dataset.getLijstMetStrings();;
-//        IDequeue<String> stringDequeue = new Deque<>(listWithStrings.size());
-//
-//        Instant startTime;
-//        long delta;
-//
-//        int expected = listWithStrings.size();
-//
-//        startTime = Instant.now();
-//        for (String element : listWithStrings) {
-//            stringDequeue.enqueFront(element);
-//        }
-//        delta = Duration.between(startTime, Instant.now()).toMillis();
-//        System.out.println("Duration for inserting String into Dequeue: " + delta + "ms");
-//
-//        int actual = stringDequeue.size();
-//
-//        Assert.assertEquals(expected, actual);
-//    }
-//
-//    @Test
-//    public void shouldInsertAtFrontOfDequeue() {
-//        testDequeue.enqueFront(10);
-//        testDequeue.enqueFront(30);
-//        testDequeue.enqueFront(50);
-//
-//        int expected = 50;
-//        int actual = testDequeue.peekFront();
-//
-//        Assert.assertEquals(expected, actual);
-//    }
-//
-//    @Test
-//    public void shouldInsertAtRearOfDequeue() {
-//        testDequeue.enqueRear(10);
-//        testDequeue.enqueRear(30);
-//        testDequeue.enqueRear(50);
-//
-//        int expected = 50;
-//        int actual = testDequeue.peekRear();
-//
-//        Assert.assertEquals(expected, actual);
-//    }
-//
-//    @Test
-//    public void shouldRemoveFromFrontOfDequeue() {
-//        testDequeue.enqueFront(10);
-//        testDequeue.enqueFront(30);
-//        testDequeue.enqueFront(50);
-//        testDequeue.enqueFront(100);
-//        testDequeue.dequeFront();
-//
-//        int expected = 50;
-//        int actual = testDequeue.peekFront();
-//
-//        Assert.assertEquals(expected, actual);
-//    }
-//
-//    @Test
-//    public void shouldRemoveFromRearOfDequeue() {
-//        testDequeue.enqueRear(10);
-//        testDequeue.enqueRear(30);
-//        testDequeue.enqueRear(50);
-//        testDequeue.enqueRear(100);
-//        testDequeue.dequeRear();
-//
-//        int expected = 50;
-//        int actual = testDequeue.peekRear();
-//
-//        Assert.assertEquals(expected, actual);
-//    }
-//
-//    @Test
-//    public void shouldPeekAtFrontOfDequeueWithoutDecrementingSize() {
-//        testDequeue.enqueFront(10);
-//        testDequeue.enqueFront(30);
-//        testDequeue.enqueFront(50);
-//        testDequeue.enqueFront(100);
-//
-//        int expected = 100;
-//        int actual = testDequeue.peekFront();
-//        int expectedSize = 4;
-//        int actualSize = testDequeue.size();
-//
-//        Assert.assertEquals(expected, actual);
-//        Assert.assertEquals(expectedSize, actualSize);
-//    }
-//
-//    @Test
-//    public void shouldPeekAtRearOfDequeueWithoutDecrementingSize() {
-//        testDequeue.enqueRear(10);
-//        testDequeue.enqueRear(30);
-//        testDequeue.enqueRear(50);
-//        testDequeue.enqueRear(100);
-//
-//        int expected = 100;
-//        int actual = testDequeue.peekRear();
-//        int expectedSize = 4;
-//        int actualSize = testDequeue.size();
-//
-//        Assert.assertEquals(expected, actual);
-//        Assert.assertEquals(expectedSize, actualSize);
-//    }
-//
-//    @Test
-//    public void shouldReturnTrueIfDequeueIsFull() {
-//        testDequeue.enqueFront(10);
-//        testDequeue.enqueFront(30);
-//        testDequeue.enqueFront(50);
-//        testDequeue.enqueFront(100);
-//
-//        boolean actual = testDequeue.isFull();
-//
-//        Assert.assertTrue(actual);
-//    }
-//
-//    @Test
-//    public void shouldReturnTrueIfDequeueIsEmpty() {
-//        boolean actual = testDequeue.isEmpty();
-//
-//        Assert.assertTrue(actual);
-//    }
-//
-//    @Test
-//    public void shouldReturnCorrectSizeOfDequeue() {
-//        testDequeue.enqueFront(10);
-//        testDequeue.enqueFront(30);
-//        testDequeue.enqueFront(50);
-//
-//        int expected = 3;
-//        int actual = testDequeue.size();
-//
-//        Assert.assertEquals(expected, actual);
-//
-//    }
-//}
+package datastructureTests;
+
+import datastructures.Deque;
+import datastructures.IDequeue;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+import java.time.Instant;
+
+public class DequeueTest {
+    JSONObject jsonObject;
+    JSONParser parser;
+    IDequeue<Integer> testDequeue;
+
+    @Before
+    public void setup() throws IOException, ParseException {
+        parser = new JSONParser();
+        ClassLoader classLoader = getClass().getClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream("datasets/sorting.json");
+        jsonObject = (JSONObject) parser.parse(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+        testDequeue = new Deque<>(4);
+    }
+
+    @Test
+    public void shouldInsertDataSetDoubleIntoDequeue() {
+        JSONArray listWithDouble = (JSONArray) jsonObject.get("lijst_float_8001");
+        IDequeue<Double> doubleDequeue = new Deque<>(listWithDouble.size());
+        Instant startTime;
+        long delta;
+
+        int actual = listWithDouble.size();
+
+        startTime = Instant.now();
+        for (Object element : listWithDouble) {
+            doubleDequeue.enqueFront((Double) element);
+        }
+        delta = Duration.between(startTime, Instant.now()).toMillis();
+        System.out.println("Duration for inserting Double into Dequeue: " + delta + "ms");
+
+        int expected = doubleDequeue.size();
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldInsertDataSetLongIntoDequeue() {
+        JSONArray listWithLong = (JSONArray) jsonObject.get("lijst_herhaald_1000");
+        IDequeue<Long> longDequeue = new Deque<>(listWithLong.size());
+        Instant startTime;
+        long delta;
+
+        int expected = listWithLong.size();
+
+        startTime = Instant.now();
+        for (Object element : listWithLong) {
+            longDequeue.enqueFront((Long) element);
+        }
+        delta = Duration.between(startTime, Instant.now()).toMillis();
+
+        System.out.println("Duration for inserting Long into Dequeue: " + delta + "ms");
+        int actual = longDequeue.size();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldInsertDataSetStringsIntoDequeue() {
+        JSONArray listWithStrings = (JSONArray) jsonObject.get("lijst_met_strings");
+        IDequeue<String> stringDequeue = new Deque<>(listWithStrings.size());
+
+        Instant startTime;
+        long delta;
+
+        int expected = listWithStrings.size();
+
+        startTime = Instant.now();
+        for (Object element : listWithStrings) {
+            stringDequeue.enqueFront((String) element);
+        }
+        delta = Duration.between(startTime, Instant.now()).toMillis();
+        System.out.println("Duration for inserting String into Dequeue: " + delta + "ms");
+
+        int actual = stringDequeue.size();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldInsertAtFrontOfDequeue() {
+        testDequeue.enqueFront(10);
+        testDequeue.enqueFront(30);
+        testDequeue.enqueFront(50);
+
+        int expected = 50;
+        int actual = testDequeue.peekFront();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldInsertAtRearOfDequeue() {
+        testDequeue.enqueRear(10);
+        testDequeue.enqueRear(30);
+        testDequeue.enqueRear(50);
+
+        int expected = 50;
+        int actual = testDequeue.peekRear();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldRemoveFromFrontOfDequeue() {
+        testDequeue.enqueFront(10);
+        testDequeue.enqueFront(30);
+        testDequeue.enqueFront(50);
+        testDequeue.enqueFront(100);
+        testDequeue.dequeFront();
+
+        int expected = 50;
+        int actual = testDequeue.peekFront();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldRemoveFromRearOfDequeue() {
+        testDequeue.enqueRear(10);
+        testDequeue.enqueRear(30);
+        testDequeue.enqueRear(50);
+        testDequeue.enqueRear(100);
+        testDequeue.dequeRear();
+
+        int expected = 50;
+        int actual = testDequeue.peekRear();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldPeekAtFrontOfDequeueWithoutDecrementingSize() {
+        testDequeue.enqueFront(10);
+        testDequeue.enqueFront(30);
+        testDequeue.enqueFront(50);
+        testDequeue.enqueFront(100);
+
+        int expected = 100;
+        int actual = testDequeue.peekFront();
+        int expectedSize = 4;
+        int actualSize = testDequeue.size();
+
+        Assert.assertEquals(expected, actual);
+        Assert.assertEquals(expectedSize, actualSize);
+    }
+
+    @Test
+    public void shouldPeekAtRearOfDequeueWithoutDecrementingSize() {
+        testDequeue.enqueRear(10);
+        testDequeue.enqueRear(30);
+        testDequeue.enqueRear(50);
+        testDequeue.enqueRear(100);
+
+        int expected = 100;
+        int actual = testDequeue.peekRear();
+        int expectedSize = 4;
+        int actualSize = testDequeue.size();
+
+        Assert.assertEquals(expected, actual);
+        Assert.assertEquals(expectedSize, actualSize);
+    }
+
+    @Test
+    public void shouldReturnTrueIfDequeueIsFull() {
+        testDequeue.enqueFront(10);
+        testDequeue.enqueFront(30);
+        testDequeue.enqueFront(50);
+        testDequeue.enqueFront(100);
+
+        boolean actual = testDequeue.isFull();
+
+        Assert.assertTrue(actual);
+    }
+
+    @Test
+    public void shouldReturnTrueIfDequeueIsEmpty() {
+        boolean actual = testDequeue.isEmpty();
+
+        Assert.assertTrue(actual);
+    }
+
+    @Test
+    public void shouldReturnCorrectSizeOfDequeue() {
+        testDequeue.enqueFront(10);
+        testDequeue.enqueFront(30);
+        testDequeue.enqueFront(50);
+
+        int expected = 3;
+        int actual = testDequeue.size();
+
+        Assert.assertEquals(expected, actual);
+
+    }
+}
--- src/test/java/datastructureTests/DoublyLinkedListTest.java
+++ src/test/java/datastructureTests/DoublyLinkedListTest.java
@@ -1,171 +1,171 @@
-//package datastructureTests;
-//
-//import datastructures.DoublyLinkedList;
-//import datastructures.ListIterator;
-//import org.json.simple.JSONObject;
-//import org.json.simple.parser.ParseException;
-//import org.junit.Assert;
-//import org.junit.Before;
-//import org.junit.Test;
-//import utils.*;
-//import java.io.IOException;
-//import java.io.InputStream;
-//import java.time.Duration;
-//import java.time.Instant;
-//import java.util.ArrayList;
-//
-//public class DoublyLinkedListTest<T> {
-//    JSONHandler jsonHandler;
-//    JSONObject jsonObject;
-//    Dataset dataset;
-//
-//    @Before
-//    public void setup() throws IOException, ParseException {
-//        ResourceReaderStrategy<InputStream> reader = new InputStreamResourceReaderStrategy();
-//        jsonHandler = new JSONHandler();
-//        dataset = new Dataset();
-//        InputStream is = reader.getFileFromResource("datasets/sorting.json");
-//        jsonObject = jsonHandler.getJSONObjectFromInputStream(is);
-//    }
-//
-//    @Test
-//    public void shouldInsertDataSetDoubleIntoDoublyLinkedList() {
-//        DoublyLinkedList<Double> doublyLinkedListDouble = new DoublyLinkedList<>();
-//        ArrayList<Double> listWithFloat = dataset.getFloat8001();
-//        ListIterator<Double> iter = doublyLinkedListDouble.getIterator();
-//        int sizeOfJsonArray = listWithFloat.size();
-//        Instant startTime;
-//        long delta;
-//
-//        startTime = Instant.now();
-//        for (Double element : listWithFloat) {
-//            iter.insertBefore(element);
-//        }
-//        delta = Duration.between(startTime, Instant.now()).toMillis();
-//
-//        System.out.println("Duration for inserting Double into Doubly Linked List: " + delta + "ms");
-//        int sizeOfFilledDoublyLinkedList = doublyLinkedListDouble.getSize();
-//
-//        Assert.assertEquals(sizeOfJsonArray, sizeOfFilledDoublyLinkedList);
-//    }
-//
-//    @Test
-//    public void shouldInsertDataSetLongIntoDoublyLinkedList() {
-//        DoublyLinkedList<Long> doublyLinkedListLong = new DoublyLinkedList<>();
-//        ArrayList<Long> listWithLong = dataset.getLijstHerhaald1000();
-//        ListIterator<Long> iter = doublyLinkedListLong.getIterator();
-//        Instant startTime;
-//        long delta;
-//
-//        int expected = listWithLong.size();
-//
-//        startTime = Instant.now();
-//        for (Long element : listWithLong) {
-//            iter.insertBefore(element);
-//        }
-//        delta = Duration.between(startTime, Instant.now()).toMillis();
-//        System.out.println("Duration for inserting Long into Doubly Linked List: " + delta + "ms");
-//
-//        int actual = doublyLinkedListLong.getSize();
-//
-//        Assert.assertEquals(expected, actual);
-//    }
-//
-//    @Test
-//    public void shouldInsertDataSetStringIntoDoublyLinkedList() {
-//        DoublyLinkedList<String> doublyLinkedListString = new DoublyLinkedList<>();
-//        ArrayList<String> listWithStrings = dataset.getLijstMetStrings();
-//        ListIterator<String> iter = doublyLinkedListString.getIterator();
-//        Instant startTime;
-//        long delta;
-//
-//        int expected = listWithStrings.size();
-//
-//        startTime = Instant.now();
-//        for (String element : listWithStrings) {
-//            iter.insertBefore(element);
-//        }
-//        delta = Duration.between(startTime, Instant.now()).toMillis();
-//        System.out.println("Duration for inserting String into Doubly Linked List: " + delta + "ms");
-//
-//        int actual = doublyLinkedListString.getSize();
-//
-//        Assert.assertEquals(expected, actual);
-//    }
-//
-//    @Test
-//    public void shouldInsertAtStart() {
-//        DoublyLinkedList<Integer> doublyLinkedList = new DoublyLinkedList<>();
-//        ListIterator<Integer> iter = doublyLinkedList.getIterator();
-//        iter.insertBefore(10);
-//        iter.insertBefore(20);
-//
-//        Integer expected = 20;
-//        Integer actual = doublyLinkedList.getFirstElement();
-//
-//        Assert.assertEquals(expected, actual);
-//    }
-//
-//    @Test
-//    public void shouldDeleteFirst() {
-//        DoublyLinkedList<Integer> doublyLinkedList = new DoublyLinkedList<>();
-//        ListIterator<Integer> iter = doublyLinkedList.getIterator();
-//        iter.insertBefore(10);
-//        iter.insertBefore(20);
-//        iter.insertBefore(50);
-//
-//        doublyLinkedList.deleteFirst();
-//
-//        Integer expected = 20;
-//        Integer actual = doublyLinkedList.getFirstElement();
-//
-//        Assert.assertEquals(expected, actual);
-//    }
-//
-//    @Test
-//    public void shouldGetCorrectCurrent() {
-//        DoublyLinkedList<Integer> doublyLinkedList = new DoublyLinkedList<>();
-//        ListIterator<Integer> iter = doublyLinkedList.getIterator();
-//        iter.insertBefore(10);
-//        iter.insertBefore(20);
-//        iter.insertBefore(50);
-//
-//        int expected = 50;
-//        int actual = iter.getCurrent().getData();
-//
-//        Assert.assertEquals(expected, actual);
-//    }
-//
-//    @Test
-//    public void shouldDeleteCorrectCurrentAndSetNewCorrectCurrent() {
-//        DoublyLinkedList<Integer> doublyLinkedList = new DoublyLinkedList<>();
-//        ListIterator<Integer> iter = doublyLinkedList.getIterator();
-//        iter.insertBefore(10);
-//        iter.insertBefore(20);
-//        iter.insertBefore(50);
-//
-//        int expectedDeletedCurrent = 50;
-//        int actualDeletedCurrent = iter.deleteCurrent();
-//
-//        int expected = 20;
-//        int actual = iter.getCurrent().getData();
-//
-//        Assert.assertEquals(expectedDeletedCurrent, actualDeletedCurrent);
-//        Assert.assertEquals(expected, actual);
-//    }
-//
-//    @Test
-//    public void shouldInsertAfter() {
-//        DoublyLinkedList<Integer> doublyLinkedList = new DoublyLinkedList<>();
-//        ListIterator<Integer> iter = doublyLinkedList.getIterator();
-//        iter.insertBefore(10);
-//        iter.insertBefore(20);
-//        iter.insertAfter(50);
-//
-//        int expected = 20;
-//        int actual = doublyLinkedList.getFirstElement();
-//
-//        Assert.assertEquals(expected, actual);
-//    }
-//
-//}
+package datastructureTests;
+
+import datastructures.DoublyLinkedList;
+import datastructures.ListIterator;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+import java.time.Instant;
+
+public class DoublyLinkedListTest<T> {
+    JSONObject jsonObject;
+    JSONParser parser;
+
+    @Before
+    public void setup() throws IOException, ParseException {
+        parser = new JSONParser();
+        ClassLoader classLoader = getClass().getClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream("datasets/sorting.json");
+        jsonObject = (JSONObject) parser.parse(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+    }
+
+    @Test
+    public void shouldInsertDataSetDoubleIntoDoublyLinkedList() {
+        DoublyLinkedList<Double> doublyLinkedListDouble = new DoublyLinkedList<>();
+        JSONArray listWithFloat = (JSONArray) jsonObject.get("lijst_float_8001");
+        ListIterator<Double> iter = doublyLinkedListDouble.getIterator();
+        int sizeOfJsonArray = listWithFloat.size();
+        Instant startTime;
+        long delta;
+
+        startTime = Instant.now();
+        for (Object element : listWithFloat) {
+            iter.insertBefore((Double) element);
+        }
+        delta = Duration.between(startTime, Instant.now()).toMillis();
+
+        System.out.println("Duration for inserting Double into Doubly Linked List: " + delta + "ms");
+        int sizeOfFilledDoublyLinkedList = doublyLinkedListDouble.getSize();
+
+        Assert.assertEquals(sizeOfJsonArray, sizeOfFilledDoublyLinkedList);
+    }
+
+    @Test
+    public void shouldInsertDataSetLongIntoDoublyLinkedList() {
+        DoublyLinkedList<Long> doublyLinkedListLong = new DoublyLinkedList<>();
+        JSONArray listWithLong = (JSONArray) jsonObject.get("lijst_herhaald_1000");
+        ListIterator<Long> iter = doublyLinkedListLong.getIterator();
+        Instant startTime;
+        long delta;
+
+        int expected = listWithLong.size();
+
+        startTime = Instant.now();
+        for (Object element : listWithLong) {
+            iter.insertBefore((Long) element);
+        }
+        delta = Duration.between(startTime, Instant.now()).toMillis();
+        System.out.println("Duration for inserting Long into Doubly Linked List: " + delta + "ms");
+
+        int actual = doublyLinkedListLong.getSize();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldInsertDataSetStringIntoDoublyLinkedList() {
+        DoublyLinkedList<String> doublyLinkedListString = new DoublyLinkedList<>();
+        JSONArray listWithStrings = (JSONArray) jsonObject.get("lijst_met_strings");
+        ListIterator<String> iter = doublyLinkedListString.getIterator();
+        Instant startTime;
+        long delta;
+
+        int expected = listWithStrings.size();
+
+        startTime = Instant.now();
+        for (Object element : listWithStrings) {
+            iter.insertBefore((String) element);
+        }
+        delta = Duration.between(startTime, Instant.now()).toMillis();
+        System.out.println("Duration for inserting String into Doubly Linked List: " + delta + "ms");
+
+        int actual = doublyLinkedListString.getSize();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldInsertAtStart() {
+        DoublyLinkedList<Integer> doublyLinkedList = new DoublyLinkedList<>();
+        ListIterator<Integer> iter = doublyLinkedList.getIterator();
+        iter.insertBefore(10);
+        iter.insertBefore(20);
+
+        Integer expected = 20;
+        Integer actual = doublyLinkedList.getFirstElement();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldDeleteFirst() {
+        DoublyLinkedList<Integer> doublyLinkedList = new DoublyLinkedList<>();
+        ListIterator<Integer> iter = doublyLinkedList.getIterator();
+        iter.insertBefore(10);
+        iter.insertBefore(20);
+        iter.insertBefore(50);
+
+        doublyLinkedList.deleteFirst();
+
+        Integer expected = 20;
+        Integer actual = doublyLinkedList.getFirstElement();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldGetCorrectCurrent() {
+        DoublyLinkedList<Integer> doublyLinkedList = new DoublyLinkedList<>();
+        ListIterator<Integer> iter = doublyLinkedList.getIterator();
+        iter.insertBefore(10);
+        iter.insertBefore(20);
+        iter.insertBefore(50);
+
+        int expected = 50;
+        int actual = iter.getCurrent().getData();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldDeleteCorrectCurrentAndSetNewCorrectCurrent() {
+        DoublyLinkedList<Integer> doublyLinkedList = new DoublyLinkedList<>();
+        ListIterator<Integer> iter = doublyLinkedList.getIterator();
+        iter.insertBefore(10);
+        iter.insertBefore(20);
+        iter.insertBefore(50);
+
+        int expectedDeletedCurrent = 50;
+        int actualDeletedCurrent = iter.deleteCurrent();
+
+        int expected = 20;
+        int actual = iter.getCurrent().getData();
+
+        Assert.assertEquals(expectedDeletedCurrent, actualDeletedCurrent);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldInsertAfter() {
+        DoublyLinkedList<Integer> doublyLinkedList = new DoublyLinkedList<>();
+        ListIterator<Integer> iter = doublyLinkedList.getIterator();
+        iter.insertBefore(10);
+        iter.insertBefore(20);
+        iter.insertAfter(50);
+
+        int expected = 20;
+        int actual = doublyLinkedList.getFirstElement();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+}
